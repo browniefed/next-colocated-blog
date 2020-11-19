449e511f842118ea3cb2e394f4e51f40c9672e79
Jason Brown <jasonbrown@Jasons-MacBook-Pro.local>
2020-11-19 09:16:32 -0800

    add

--- pages/blog/[...page].tsx
+++ pages/blog/[...page].tsx
@@ -1,6 +1,37 @@
 import dynamic from "next/dynamic";
 import Layout from "../../layout";
-import { getTutorialPaths, TUTORIAL_PATH } from "../../lib/mdx";
+import glob from "glob";
+import { join, extname, basename } from "path";
+
+export const getMDXFiles = (src): Promise<string[]> => {
+  return new Promise((resolve, reject) => {
+    glob(src + "/**/*.mdx", (err, res) => {
+      if (err) {
+        reject();
+        return;
+      }
+
+      resolve(res);
+    });
+  });
+};
+
+export const getPathParam = (path, directory) => {
+  const bits = path.replace(directory, "").split("/").filter(Boolean).reverse();
+  const [last, ...all] = bits;
+  return [basename(last, extname(last)), ...all].reverse();
+};
+
+export const getTutorialPaths = async () => {
+  const tutorialsDirectory = join(process.cwd(), TUTORIAL_PATH);
+  const pathResults = await getMDXFiles(tutorialsDirectory);
+  const tutorialPaths = pathResults?.map((path) => {
+    return getPathParam(path, tutorialsDirectory);
+  });
+  return tutorialPaths;
+};
+
+const TUTORIAL_PATH = "content/tutorials";
 
 export async function getStaticPaths() {
   const tutorialPaths = await getTutorialPaths();
