0d55bd30b50a55d9df2e051785054c4388329eb2
Jason Brown <jasonbrown@Jasons-MacBook-Pro.local>
2020-11-19 09:15:16 -0800

    Add

--- next.config.js
+++ next.config.js
@@ -14,4 +14,13 @@ const withMDX = require("@next/mdx")({
 
 module.exports = withMDX({
   pageExtensions: ["ts", "tsx", "mdx"],
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.node = {
+        fs: "empty",
+      };
+    }
+
+    return config;
+  },
 });
--- pages/blog/[...page].tsx
+++ pages/blog/[...page].tsx
@@ -1,36 +1,9 @@
 import dynamic from "next/dynamic";
 import Layout from "../../layout";
-import glob from "glob";
-import { join, extname, basename } from "path";
-
-export const TUTORIAL_PATH = "content/tutorials";
-
-export const getMDXFiles = (src): Promise<string[]> => {
-  return new Promise((resolve, reject) => {
-    glob(src + "/**/*.mdx", (err, res) => {
-      if (err) {
-        reject();
-        return;
-      }
-
-      resolve(res);
-    });
-  });
-};
-
-export const getPathParam = (path, directory) => {
-  const bits = path.replace(directory, "").split("/").filter(Boolean).reverse();
-  const [last, ...all] = bits;
-  return [basename(last, extname(last)), ...all].reverse();
-};
+import { getTutorialPaths, TUTORIAL_PATH } from "../../lib/mdx";
 
 export async function getStaticPaths() {
-  const tutorialsDirectory = join(process.cwd(), TUTORIAL_PATH);
-  const pathResults = await getMDXFiles(tutorialsDirectory);
-  const tutorialPaths = pathResults?.map((path) => {
-    return getPathParam(path, tutorialsDirectory);
-  });
-
+  const tutorialPaths = await getTutorialPaths();
   const paths = tutorialPaths.map((path) => {
     return {
       params: {
